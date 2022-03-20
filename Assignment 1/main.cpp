149741fdf21be3fcc442814ddffbe2621d365e18
Chandra Vamsi <vamsi.tvcv901@gmail.com>
2022-03-20 19:43:18 +0530

    Added main.cpp for Sweep Line algo implementation

---
+++ Assignment 1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include ".\headers\EQNode.h"
+#include ".\headers\EventQueue.h"
+#include ".\headers\Point.h"
+#include ".\headers\SLine.h"
+#include ".\headers\SNode.h"
+#include ".\headers\status.h"
+#include "EQBalancedBST.cpp"
+#include "SBalancedBST.cpp"
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
