15b911dd70719c309c6fa45ab91158c4dc86506b
Chandra Vamsi <vamsi.tvcv901@gmail.com>
2022-03-21 10:18:21 +0530

    Updated main.cpp

--- Assignment 1/main.cpp
+++ Assignment 1/main.cpp
@@ -12,16 +12,6 @@ using namespace std;
 
 int main()
 {
-    Point p1, p2, p3;
-    p1.x = p2.x = 5.1234, p1.y = p2.y = 0.6789;
-    cout << (p1 == p2) << '\n';
-    cout << (p1 < p2) << '\n';
-    cout << (p1 > p2) << '\n';
-
-    p3 = p1;
-    cout << (p1 == p3) << '\n';
-    cout << (p1 < p3) << '\n';
-    cout << (p1 > p3) << '\n';
 
     return 0;
 }
