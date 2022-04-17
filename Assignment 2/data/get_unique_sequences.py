2a3569b1536521d61194015c8ab6640a71256037
Chandra Vamsi <vamsi.tvcv901@gmail.com>
2022-04-17 21:24:25 +0530

    Deleted get_unique_sequences.py

--- Assignment 2/data/get_unique_sequences.py
+++
@@ -1,9 +0,0 @@
-f = open('data\\input_rna_sequences.txt', 'r')
-sequences = f.readlines()
-f.close()
-
-unique_sequences = set(sequences)
-f = open('data\\unique_rna_sequences.txt', 'w')
-for sequence in unique_sequences:
-    f.write(sequence)
-f.close()
